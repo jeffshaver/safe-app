4f28d1a87737f6e11382ffc186fda9315abeaf8c
David Johnson <david.johnson@vistronix.com>
2016-02-23 20:39:14 +0000

    adding sample routes/fixtures

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ app.use(express.static('dist'))
 app.use(express.static('node_modules'))
 app.use('config.js', express.static('config.js'))
 
+// FIXTURE ROUTES FOR TESTING
+require('./test/routes')(app);
+
 app.get('/*', function (req, res) {
   res.sendFile(__dirname + '/src/html/index.html')
 })
