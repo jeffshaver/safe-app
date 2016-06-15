9bde5a1d7330d3354fc1f4a48c7bad28def90761
Patrick <patrick.leister@vistronix.com>
2016-06-15 13:35:34 +0000

    closes #123. incoporating in drill down from safe-framework

--- src/js/components/ChartComponent.jsx
+++ src/js/components/ChartComponent.jsx
@@ -1,54 +1,71 @@
 import * as Components from 'safe-framework'
-import React, {PropTypes} from 'react'
+import React, {Component, PropTypes} from 'react'
 
-export const ChartComponent = ({data, metadata, type}) => {
-  const {visualizationParams: params = {}} = metadata
-  const {series, yAxis = 'Count', type: xAxisType} = params
-  const xAxisScale = xAxisType !== 'timeline' ? {} : {
-    time: {
-      tooltipFormat: 'dddd, MMMM Do YYYY, hh:mm'
-    },
-    type: 'time'
+export class ChartComponent extends Component {
+  static propTypes = {
+    data: PropTypes.array.isRequired,
+    metadata: PropTypes.object.isRequired,
+    type: PropTypes.string.isRequired
   }
-  const options = {
-    scales: {
-      xAxis: [xAxisScale],
-      yAxes: [{
-        scaleLabel: {
-          display: false,
-          labelString: ''
-        }
-      }]
+
+  render () {
+    const {data, metadata, type} = this.props
+    const {visualizationParams: params = {}} = metadata
+    const {series, yAxis = 'Count', type: xAxisType, drillDownFieldName} = params
+    const xAxisScale = xAxisType !== 'timeline' ? {} : {
+      time: {
+        tooltipFormat: 'dddd, MMMM Do YYYY, hh:mm'
+      },
+      type: 'time'
     }
-  }
-  const chartData = {
-    data,
-    xAxis: ['Value']
-  }
-
-  if (series) {
-    chartData.ySeriesField = 'Details'
-    chartData.ySeriesFieldName = 'Value'
-    chartData.ySeriesFieldValue = yAxis
-  } else {
-    chartData.yAxis = [yAxis]
-    options.legend = {
-      display: false
+    const options = {
+      scales: {
+        xAxis: [xAxisScale],
+        yAxes: [{
+          scaleLabel: {
+            display: false,
+            labelString: ''
+          }
+        }]
+      }
     }
+    const chartData = {
+      data,
+      xAxis: ['Value']
+    }
+
+    if (series) {
+      chartData.ySeriesField = 'Details'
+      chartData.ySeriesFieldName = 'Value'
+      chartData.ySeriesFieldValue = yAxis
+    } else {
+      chartData.yAxis = [yAxis]
+      options.legend = {
+        display: false
+      }
+    }
+
+    const ChartType = Components[`${type}Chart`]
+
+    return (
+      <ChartType
+        data={chartData}
+        drilldown={drillDownFieldName != null}
+        options={options}
+        ref='chart'
+        onClick={(dataItem, series, datasetIndex) => {
+          if (!drillDownFieldName) {
+            return
+          }
+
+          const {chart} = this.refs
+
+          chart.drilldown({
+            ...chartData,
+            data: dataItem[drillDownFieldName]
+          })
+        }}
+      />
+    )
   }
-
-  const ChartType = Components[`${type}Chart`]
-
-  return (
-    <ChartType
-      data={chartData}
-      options={options}
-    />
-  )
-}
-
-ChartComponent.propTypes = {
-  data: PropTypes.array.isRequired,
-  metadata: PropTypes.object.isRequired,
-  type: PropTypes.string.isRequired
 }
