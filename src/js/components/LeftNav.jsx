868314a5d7062b182ae6e7753b6e4b6e648e27af
jpkeyw <jpolirer@keywcorp.com>
2016-06-23 12:37:49 +0000

    Closes #139 - add help menu to leftnav

--- src/js/components/LeftNav.jsx
+++ src/js/components/LeftNav.jsx
@@ -1,4 +1,4 @@
-import {applicationName} from '../../../config'
+import ActionHelp from 'material-ui/svg-icons/action/help'
 import Avatar from 'material-ui/Avatar'
 import {browserHistory} from 'react-router'
 import Divider from 'material-ui/Divider'
@@ -6,7 +6,18 @@ import Drawer from 'material-ui/Drawer'
 import ListItem from 'material-ui/List/ListItem'
 import {logoStyle} from '../styles/common'
 import {routes} from '../constants'
+import {applicationName, helpMenuItems} from '../../../config'
 import React, {Component} from 'react'
+
+const openInNewTab = (url) => {
+  const win = window.open(url, '_blank')
+
+  if (!win) {
+    return
+  }
+
+  win.focus()
+}
 
 export class LeftNav extends Component {
   render () {
@@ -43,6 +54,30 @@ export class LeftNav extends Component {
               </ListItem>
             )
           })
+        }
+        {
+          helpMenuItems.length > 0
+          ? (
+            <ListItem
+              leftAvatar={
+                <Avatar
+                  icon={<ActionHelp />}
+                />
+              }
+              nestedItems={
+                helpMenuItems.map((helpItem, i) => (
+                  <ListItem
+                    key={i}
+                    primaryText={helpItem.title}
+                    onTouchTap={() => {openInNewTab(helpItem.link)}}
+                  />
+                ))
+              }
+              primaryText='Help'
+              primaryTogglesNestedList={true}
+            />
+          )
+          : ('')
         }
       </Drawer>
     )
