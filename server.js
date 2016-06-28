a6a067794529939c730ccc92c160bd38648d53ac
Jeffrey Shaver <jeff.e.shaver@gmail.com>
2016-06-28 19:34:45 +0000

    closes #159. refactor server to fix bugs and remove mongo

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const spawn = require('child_process').spawn
 const StringDecoder = require('string_decoder').StringDecoder
 const decoder = new StringDecoder(('utf8'))
 const readline = require('readline')
-const fs = require('fs')
+const http = require('http')
 const https = require('https')
 const express = require('express')
 const compression = require('compression')
@@ -12,8 +12,47 @@ const cookieParser = require('cookie-parser')
 const errorHandler = require('errorhandler')
 const serveStatic = require('serve-static')
 const config = require('./config')
+const fs = require('fs')
 const app = express()
 const log = (msg) => (process.stdout.write(`${msg}\n`))
+const createServerOptions = (config, app) => {
+  const {
+    protocol,
+    serverKey,
+    serverCertificate,
+    serverCertificateAuthority
+  } = config
+
+  if (protocol === 'http') return [app]
+
+  if (!serverKey || !serverCertificate || !serverCertificateAuthority) {
+    const forgottenKeys = ['serverKey', 'serverCertificate', 'serverCertificateAuthority'].filter((key) => {
+      return !config[key]
+    })
+
+    log('[WARN]: You\'re config is set to use https for the server,')
+    log(`[WARN]: but you forgot to pass in ${forgottenKeys.join(', ')}`)
+
+    return [app]
+  }
+
+  return [{
+    key: fs.readFileSync(serverKey),
+    cert: fs.readFileSync(serverCertificate),
+    ca: fs.readFileSync(serverCertificateAuthority),
+    requestCert: true,
+    rejectUnauthorized: false,
+    secureProtocol: 'TLSv1_2_method'
+  }, app]
+}
+const createServer = (config, args) => {
+  const {port} = config
+  const protocol = args.length === 2 ? https : http
+
+  protocol.createServer(...args).listen(port, () => {
+    log(`server listning on port ${port}`)
+  })
+}
 
 app.use(compression())
 app.use(morgan('dev'))
@@ -69,57 +108,9 @@ if (app.get('env') === 'development') {
   })
 }
 
-if (config.mongoUri) {
-  const session = require('express-session')
-  const connectMongo = require('connect-mongo')
-  const MongoStore = connectMongo(session)
-  const mongoOptions = {
-    url: config.mongoUri,
-    pool: true
-  }
+const serverOptions = createServerOptions(config, app)
 
-  app.use(session({
-    cookie: {
-      maxAge: 60 * 60 * 12 * 1000
-    },
-    resave: false,
-    saveUninitialized: true,
-    secret: config.sessionSecret,
-    store: new MongoStore(mongoOptions)
-  }))
-
-  const mongoClient = require('mongodb').MongoClient
-
-  mongoClient.connect(config.mongoUri, (error, database) => {
-    if (error) {
-      throw new Error(error)
-    }
-
-    log('Connected to Mongo')
-
-    app.set('db', database)
-
-    const serverOptions = {
-      key: fs.readFileSync(config.serverKey),
-      cert: fs.readFileSync(config.serverCertificate),
-      ca: fs.readFileSync(config.serverCertificateAuthority),
-      requestCert: true,
-      rejectUnauthorized: true,
-      secureProtocol: 'TLSv1_2_method'
-    }
-
-    https.createServer(serverOptions, app).listen(config.port, () => {
-      log(`server listning on port ${config.port}`)
-    })
-  })
-} else {
-  app.listen(config.port, process.env.IP, () => {
-    log(`server listning on port ${config.port}`)
-  })
-}
-
-// FIXTURE ROUTES FOR TESTING
-require('./test/routes')(app)
+createServer(config, serverOptions)
 
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, '/src/html/index.html'))
