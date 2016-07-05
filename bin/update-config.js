79f75a3e552f72bcd80dbf4bb0146e9596b9f81a
Jeffrey Shaver <jeff.e.shaver@gmail.com>
2016-07-05 15:47:20 +0000

    add support for nested arrays and objects in update:config

--- bin/update-config.js
+++ bin/update-config.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 const jsesc = require('jsesc')
+const is = require('is')
 const defaultConfig = JSON.parse(fs.readFileSync('default-config.json', 'utf8'))
 let existingConfig
 
@@ -14,7 +15,7 @@ try {
 const getValue = (value) => {
   let newValue = JSON.stringify(value)
 
-  if (typeof newValue === 'string') {
+  if (is.string(newValue)) {
     newValue = jsesc(newValue)
   }
 
@@ -22,9 +23,31 @@ const getValue = (value) => {
 }
 
 const fullConfig = Object.assign({}, defaultConfig, existingConfig)
-const keys = Object.keys(fullConfig)
-const newConfig = 'module.exports = {' + keys.map((key, index) => (
-  (index !== 0 ? ',' : '') + `\n  ${key}: ${getValue(fullConfig[key])}`
-)).join('').replace(/"/g, '\'') + '\n}'
+const generateArray = (array, level = 1) => {
+  if (array.length === 0) return '[]'
+
+  return `[\n${array.map((item, index) => {
+    return `${index !== 0 ? ',\n' : ''}${'  '.repeat(level)}${getValue(item)}`
+  }).join('')}\n${'  '.repeat(level - 1)}]`
+}
+const generateObject = (config, level = 1) => {
+  const keys = Object.keys(config)
+
+  return '{' +
+    keys.map((key, index) => {
+      if (is.object(config[key])) {
+        return `\n${'  '.repeat(level)}${key}: ${generateObject(config[key], level + 1)}`
+      }
+
+      if (is.array(config[key])) {
+        return `\n${'  '.repeat(level)}${key}: ${generateArray(config[key], level + 1)}`
+      }
+
+      return `\n${'  '.repeat(level)}${key}: ${getValue(config[key])}`
+    }) +
+    `\n${'  '.repeat(level - 1)}}`
+}
+
+const newConfig = 'module.exports = ' + generateObject(fullConfig).replace(/"/g, '\'')
 
 fs.writeFileSync('config.js', newConfig)
